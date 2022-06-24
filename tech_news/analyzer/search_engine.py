ddbb83d441649de7c6840cd7bda01639a477d4b9
LucasAccurcio <lucas.accurcio@gmail.com>
2022-06-24 15:58:55 -0300

    refactor: :bug: add lib verificacao de data e remove codigos que fazia a verificacao

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+from datetime import datetime
 
 
 # Requisito 6
@@ -15,65 +16,45 @@ def search_by_title(title):
 
 
 # função para remover o 0 do dia < 10
-def fix_day(day):
+def remove_zero_from_day(day):
     if int(day) > 9:
         return day
     else:
-        if(len(day) == 2):
+        if len(day) == 2:
             return day[1]
 
 
-# função para verificar se a data está no formato correto
-# e retornar a data por extenso
-def check_date(date):
+# função para retornar a data por extenso
+def write_date(date):
     list_date = date.split("-")
 
-    if len(list_date) == 3:
-        if (
-            list_date[0].isdigit()
-            and list_date[1].isdigit()
-            and list_date[2].isdigit()
-            and len(list_date[0]) == 4
-            and len(list_date[1]) == 2
-            and len(list_date[2]) == 2
-        ):
-            if (
-                int(list_date[2]) <= 31
-                and 0 < int(list_date[1]) <= 12
-                and 1900 < int(list_date[0]) <= 2999
-            ):
-                meses = [
-                    "janeiro",
-                    "fevereiro",
-                    "março",
-                    "abril",
-                    "maio",
-                    "junho",
-                    "julho",
-                    "agosto",
-                    "setembro",
-                    "outubro",
-                    "novembro",
-                    "dezembro",
-                ]
-                list_date[2] = fix_day(list_date[2])
-                return (
-                    f"{list_date[2]}"
-                    + f" de {meses[int(list_date[1]) - 1]}"
-                    + f" de {list_date[0]}"
-                )
-            else:
-                raise ValueError
-        else:
-            raise ValueError
-    else:
-        raise ValueError
+    meses = [
+        "janeiro",
+        "fevereiro",
+        "março",
+        "abril",
+        "maio",
+        "junho",
+        "julho",
+        "agosto",
+        "setembro",
+        "outubro",
+        "novembro",
+        "dezembro",
+    ]
+    list_date[2] = remove_zero_from_day(list_date[2])
+    return (
+        f"{list_date[2]}"
+        + f" de {meses[int(list_date[1]) - 1]}"
+        + f" de {list_date[0]}"
+    )
 
 
 # Requisito 7
 def search_by_date(date):
     try:
-        written_date = check_date(date)
+        datetime.fromisoformat(date)
+        written_date = write_date(date)
         search_data = search_news(
             {"timestamp": {"$regex": f"{written_date}", "$options": "i"}}
         )
@@ -84,10 +65,7 @@ def search_by_date(date):
             )
         return response
     except ValueError:
-        print("Data inválida")
-
-
-search_by_date("20521-04-04")
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
@@ -101,9 +79,6 @@ def search_by_tag(tag):
             (data["title"], data["url"]),
         )
     return response
-
-
-search_by_tag("Tecnologia")
 
 
 # Requisito 9
