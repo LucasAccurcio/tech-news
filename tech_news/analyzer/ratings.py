0f072e99e371069ed22f4c958190f586f3d35a25
LucasAccurcio <lucas.accurcio@gmail.com>
2022-06-23 21:13:00 -0300

    feat: :sparkles: requisito 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,27 @@
+import pymongo
+from tech_news.database import get_collection
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    collection = get_collection()
+    data_sorted = list(
+            collection.find({}, {"title": 1, "url": 1, "_id": False}).sort([
+                ("comments_count", pymongo.DESCENDING),
+                ("title", pymongo.ASCENDING)
+            ]).limit(5)
+        )
+
+    response = []
+
+    for data in data_sorted:
+        response.append(
+            (data["title"], data["url"]),
+        )
+    return response
+
+
+top_5_news()
 
 
 # Requisito 11
