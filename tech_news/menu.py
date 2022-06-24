17e23dea195a74f2539d426c69d0c84c7cc777c6
LucasAccurcio <lucas.accurcio@gmail.com>
2022-06-24 17:53:47 -0300

    feat: :sparkles: requisito 12

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,3 +1,42 @@
+def print_menu():
+    texto = (
+        "Selecione uma das opções a seguir:\n"
+        " 0 - Popular o banco com notícias;\n"
+        " 1 - Buscar notícias por título;\n"
+        " 2 - Buscar notícias por data;\n"
+        " 3 - Buscar notícias por tag;\n"
+        " 4 - Buscar notícias por categoria;\n"
+        " 5 - Listar top 5 notícias;\n"
+        " 6 - Listar top 5 categorias;\n"
+        " 7 - Sair."
+    )
+    try:
+        option = int(input(texto))
+        return option
+    except ValueError:
+        print("Opção inválida!\n\n")
+
+
 # Requisito 12
 def analyzer_menu():
-    """Seu código deve vir aqui"""
+    exit = False
+    while (exit is False):
+        option = print_menu()
+        if option == 0:
+            print("Digite quantas notícias serão buscadas:")
+        elif option == 1:
+            print("Digite o título:")
+        elif option == 2:
+            print("Digite a data no formato aaaa-mm-dd:")
+        elif option == 3:
+            print("Digite a tag:")
+        elif option == 4:
+            print("Digite a categoria:")
+        elif option == 5:
+            print("Listar top 5 notícias:")
+        elif option == 6:
+            print("Listar top 5 categorias:")
+        elif option == 7:
+            exit = True
+        else:
+            exit = True
